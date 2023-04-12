289e2a075d707d6a6df40d6a12ea5203db5eeb9f
Mattis3858 <109306011@nccu.edu.tw>
2023-04-12 15:06:54 +0800

    Change the devStart to dev and add the process environment port in  the server.js file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
@@ -6,4 +7,5 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.listen(8080);
+const port = parseInt(process.env.PORT, 10) || 8080;
+app.listen(port);
