34f768229c48932b54a6a2d262db32905c77f507
Mattis <mattis3858@yahoo.com.tw>
2023-04-14 15:44:13 +0800

    add layout for default view of site

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes/index');
+
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+app.use('/', indexRouter);
 
-const port = parseInt(process.env.PORT, 10) || 8080;
-app.listen(port);
+app.listen(process.env.PORT || 3000);
